180e4fcc3d6c37e461217bc1e396d08d20400951
ahmdeltoky03 <ahmdeltoky4@gmail.com>
2024-04-20 14:44:15 +0200

    lab2_day3

---
+++ day_3/Lab_2/SQLQuery1.sql
@@ -0,0 +1,41 @@
+--Example 2
+Zero Normal Form
+
+Dept # ,Dept Name, Location,Mgr Name,Mgr ID No,Tel Extn,Cust #,Cust Name,
+Date of Complaint,Nature of Complaint  
+
+First Normal Form 
+
+Department
+Dept # , Dept Name , Location , Mgr Name , Mgr ID No , Tel Extn
+
+Customer
+Cust # , Cust Name 
+
+Dept_Cust
+Dept # , Cust # , Date of Complaint , Nature of Complaint
+
+Second Normal Form
+
+Department
+Dept # , Dept Name , Location , Mgr Name , Mgr ID No , Tel Extn
+
+Customer
+Cust # , Cust Name 
+
+Dept_Cust
+Dept # , Cust # , Date of Complaint , Nature of Complaint
+
+Third Normal From 
+
+Customer
+Cust # , Cust Name 
+
+Dept_Cust
+Dept # , Cust # , Date of Complaint , Nature of Complaint
+
+Department
+Dept # , Dept Name , Location , Mgr ID No , Tel Extn
+
+Manger
+Mgr ID No # , Mgr name
